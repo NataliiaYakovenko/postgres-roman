cf1f3dd46a1b73acd966e683459e01d0a990d906
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-06-12 22:32:47 +0300

    Creating tabel users

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,2 +1,12 @@
-CREATE TABLE test();
---ctrl -e(2)CREATE TABLE test();
+DROP TABLE test;
+--Як виконати запит Ctrl E (2 рази)
+/*
+ char(5)   передаємо "1,2,3,4,5,6,7" - передається помилка
+ varchar(5)  передаємо "1,2,3,4,5,6,7" - передається помилка
+ text     max 1GB
+ */
+CREATE TABLE users(
+    first_name varchar(64),
+    last_name varchar(64),
+    biography text
+);
