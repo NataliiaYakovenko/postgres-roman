080264e13d7bcf3080b046e4742674f61f7734c5
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-06-13 21:08:22 +0300

    insert severel users

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -9,3 +9,46 @@ CREATE TABLE users(
     foot_size smallint,
     height numeric(5, 2)
 );
+INSERT INTO users
+VALUES(
+        'Nataliia',
+        'Yakovenko',
+        'I live in Zaporizhzha',
+        'female',
+        true,
+        '1983.10.14',
+        36,
+        158.02
+    );
+INSERT INTO users
+VALUES (
+        'Lidia',
+        'Yakovenko',
+        'I live in Zaporizhzha',
+        'female',
+        true,
+        '1954.01.07',
+        37,
+        159.09
+    ),
+    (
+        'Roll',
+        'Simonyn',
+        'I live in Armeniia',
+        'male',
+        true,
+        '1998.12.27',
+        41,
+        178.32
+    );
+INSERT INTO users
+VALUES(
+        'Bob',
+        'Bobenko',
+        'I live in Zaporizhzha',
+        'male',
+        false,
+        '1999.06.17',
+        NULL,
+        NULL
+    );
