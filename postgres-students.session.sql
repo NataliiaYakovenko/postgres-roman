a32ff0e52bf73b30d867dd7c2fa76ac88f01e39b
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-06-13 22:05:08 +0300

    CONSTRAINT

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,13 +1,13 @@
 DROP TABLE users;
 CREATE TABLE users(
-    first_name VARCHAR(64) NOT NULL CHECK(first_name != ''),
-    last_name VARCHAR(64) NOT NULL CHECK(last_name != ''),
+    first_name VARCHAR(64) NOT NULL CONSTRAINT first_name_not_empty CHECK(first_name != ''),
+    last_name VARCHAR(64) NOT NULL CONSTRAINT last_name_not_empty CHECK(last_name != ''),
     email VARCHAR(64) NOT NULL CHECK(email != '') UNIQUE,
     gender VARCHAR(30) NOT NULL CHECK(gender != ''),
     is_subscribe boolean NOT NULL,
     birthday date,
     foot_size smallint,
-    height numeric(5, 2)
+    height numeric(5, 2) CONSTRAINT too_height_user CHECK(height < 2.4)
 );
 INSERT INTO users
 VALUES(
@@ -18,16 +18,16 @@ VALUES(
         true,
         '1983.10.14',
         36,
-        158.02
+        1.58
     );
 INSERT INTO users
 VALUES(
         'Nataliia',
         'Yakovenko',
-        'yakovenkonatali999@gmail.com',
+        'natali999@gmail.com',
         'female',
         true,
         '1983.10.14',
         36,
-        158.02
+        2.5
     );
