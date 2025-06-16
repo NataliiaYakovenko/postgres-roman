37df66185fb5dbcd8831dc0102789ea113eee341
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-06-16 22:48:49 +0300

    Add table books

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -34,12 +34,38 @@ VALUES(
     );
 ALTER TABLE priducts
 ADD COLUMN quantity INTEGER;
-
 ALTER TABLE priducts
 ADD CONSTRAINT "products_quantity_check" CHECK(quantity >= 0);
+ALTER TABLE priducts DROP CONSTRAINT "products_quantity_check";
+ALTER TABLE priducts DROP COLUMN quantity;
+// --------------------------------------------------------------------------------------------
 
-ALTER TABLE priducts
-DROP CONSTRAINT "products_quantity_check";
 
-ALTER TABLE priducts
-DROP COLUMN quantity;
+CREATE TABLE books(
+    id SERIAL PRIMARY KEY,
+    author VARCHAR(260),
+    title VARCHAR(200),
+    year_publication SMALLINT CONSTRAINT year_publica_Later_current_date CHECK(year_publication <= EXTRACT(YEAR FROM CURRENT_DATE)), 
+    publoshing_house VARCHAR(260) CONSTRAINT publoshing_house_not_empty CHECK(publoshing_house != ''),
+    category VARCHAR(250) CONSTRAINT category_not_empty CHECK(category != ''),
+    description TEXT, 
+    quantity INTEGER,
+    is_published BOOLEAN
+);
+
+DROP TABLE books;
+
+INSERT INTO books(author,title,quantity)
+VALUES('Taras Shevchenko', 'Dream', 300);
+
+ALTER TABLE books
+ADD CONSTRAINT "quantity_not_negative" CHECK(quantity >= 0);
+
+INSERT INTO books(author,title,quantity)
+VALUES('Shevchenko', 'Home', -300);
+
+ALTER TABLE books
+ADD CONSTRAINT "author_title" UNIQUE(author, title);
+
+INSERT INTO books(author,title,quantity)
+VALUES('Taras', 'D', 800);
