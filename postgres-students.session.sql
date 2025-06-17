a7af47c2a850dc7a615d8adfb1aa47dd66fb306a
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-06-17 22:14:42 +0300

    Add багато до багатьох

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -13,3 +13,26 @@ DROP TABLE orders;
 
 ALTER TABLE users
 ADD COLUMN id SERIAL PRIMARY KEY;
+
+CREATE TABLE orders_to_products(
+    product_id INT REFERENCES priducts(id),
+    order_id INT REFERENCES orders(id),
+    quntity INT,
+    PRIMARY KEY(order_id,product_id)
+);
+
+-- Оформлення замовлення для якогось юзера
+--1 створили замовлення
+INSERT INTO orders (costumer_id)
+VALUES (1);
+
+--2 Наповнити замовлення
+INSERT INTO orders_to_products (
+    product_id,
+    order_id,
+    quntity
+  )
+VALUES
+ (1,1,1),
+ (2,1,2),
+ (3,1,1);
