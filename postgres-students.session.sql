8a8ca830dfe838cd3e5e1fbaf3ed1bb18f8a3ada
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-06-18 21:52:30 +0300

    Add home task chats_to_userss

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,38 +1,30 @@
-DROP TABLE books,
-coordinates,
-messages;
+CREATE TABLE userss(
+   id SERIAL PRIMARY KEY,
+   nickname  VARCHAR (100) CONSTRAINT nickname_not_empty CHECK(nickname != '') NOT NULL
+);
+
+CREATE TABLE chats(
+     id SERIAL PRIMARY KEY,
+    name  VARCHAR (260) CONSTRAINT name_not_empty CHECK(name != '') NOT NULL,
+    owner_id INT REFERENCES userss(id)  ,
+    created_at  TIMESTAMP CONSTRAINT created_at_not_empty CHECK(created_at <=     
+            CURRENT_DATE) NOT NULL DEFAULT CURRENT_DATE
+);
 
 
-CREATE TABLE orders(
+CREATE TABLE chats_to_userss(
+      chat_id INT REFERENCES chats(id),
+      user_id INT REFERENCES userss(id),
+      join_at TIMESTAMP CONSTRAINT join_at_not_empty CHECK(join_at <= CURRENT_DATE) NOT NULL DEFAULT CURRENT_DATE,
+     PRIMARY KEY (chat_id, user_id )
+);
+
+CREATE TABLE messages(
     id SERIAL PRIMARY KEY,
-    create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    costumer_id INT REFERENCES users(id)
+    body TEXT CONSTRAINT body_not_empty CHECK(body != '') NOT NULL,
+    created_at TIMESTAMP CONSTRAINT created_at_not_empty CHECK(created_at <= CURRENT_DATE) NOT NULL DEFAULT CURRENT_DATE,
+    author_id INT REFERENCES userss(id),
+    chat_id INT REFERENCES chats(id)
 );
 
-DROP TABLE orders;
-
-ALTER TABLE users
-ADD COLUMN id SERIAL PRIMARY KEY;
-
-CREATE TABLE orders_to_products(
-    product_id INT REFERENCES priducts(id),
-    order_id INT REFERENCES orders(id),
-    quntity INT,
-    PRIMARY KEY(order_id,product_id)
-);
-
--- Оформлення замовлення для якогось юзера
---1 створили замовлення
-INSERT INTO orders (costumer_id)
-VALUES (1);
-
---2 Наповнити замовлення
-INSERT INTO orders_to_products (
-    product_id,
-    order_id,
-    quntity
-  )
-VALUES
- (1,1,1),
- (2,1,2),
- (3,1,1);
+DROP TABLE messages;
