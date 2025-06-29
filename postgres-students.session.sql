5433052f7ec0339eeffbfef4e731c8c0d8d4864b
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-06-29 14:54:57 +0300

    Add зв'язок 1 до 1

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,59 +1,15 @@
-CREATE DATABASE chats;
+-- 1 до 1 
 
-DROP TABLE users;
-CREATE TABLE users(
-   id SERIAL PRIMARY KEY,
-   nickname  VARCHAR (100) CHECK(nickname != '') NOT NULL
-);
-INSERT INTO users(nickname)
-VALUES ('Natakia'),
-        ('Jon'),
-        ('Pheabi');
-
-
-
-DROP TABLE chats;
-CREATE TABLE chats(
-     id SERIAL PRIMARY KEY,
-    name  VARCHAR (260) CHECK(name != '') NOT NULL,
-    owner_id INT REFERENCES users(id)  ,
-    created_at  TIMESTAMP  DEFAULT CURRENT_TIMESTAMP
-);
-INSERT INTO chats(name, owner_id)
-VALUES ('Super chat', 1),
-       ('Travel chat', 2),
-       ('Shop chat', 3);
-
-
-DROP TABLE chats_to_users;
-CREATE TABLE chats_to_users(
-      chat_id INT REFERENCES chats(id),
-      user_id INT REFERENCES users(id),
-      join_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-     PRIMARY KEY (chat_id, user_id )
-);
-INSERT INTO chats_to_users(chat_id,user_id)
-VALUES (1, 1),
-       (2, 2),
-       (3, 3);
-
-INSERT INTO chats_to_users(chat_id,user_id)
-VALUES (1, 2);      
-
-
-DROP TABLE messages;
-CREATE TABLE messages(
+CREATE TABLE coaches(
     id SERIAL PRIMARY KEY,
-    body TEXT CHECK(body != '') NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    is_read BOOLEAN NOT NULL DEFAULT false,
-    -- author_id INT REFERENCES chats_to_users(user_id),  -- варіант 1
-    -- chat_id INT REFERENCES chats_to_users(chat_id)     -- варіант 1
-    author_id INT,                                             -- варіант 2
-    chat_id INT ,                                               -- варіант 2
-    FOREIGN KEY (author_id, chat_id) REFERENCES chats_to_users(user_id, chat_id)  -- варіант 2
+    name VARCHAR(250) CHECK(name != '') NOT NULL  
 );
 
-INSERT INTO messages(body,author_id,chat_id)
-VALUES('Go to drink coffee', 1, 1),
-      ('Go', 2, 1);
+CREATE TABLE teams(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(250) CHECK(name != '') NOT NULL,
+    coache_id INT REFERENCES coaches(id)
+);
+
+ALTER TABLE coaches
+ADD COLUMN team_id INT REFERENCES teams(id);
