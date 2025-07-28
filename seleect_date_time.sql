5fcc5e4aea547e16bf5395dbd27cbb374b8fd65a
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-07-28 14:25:35 +0300

    Add підзапити

--- seleect_date_time.sql
+++ seleect_date_time.sql
@@ -5,59 +5,119 @@ WHERE (gender = 'male' AND is_subscribe)
 
 UPDATE users
 SET birthday = '2005-05-05'
-WHERE (gender = 'male' AND is_subscribe = 'false')
+WHERE (gender = 'male'AND is_subscribe = 'false')
 
 UPDATE users
 SET birthday = '209-09-09'
-WHERE (gender = 'female' AND is_subscribe)
+WHERE (gender = 'female'AND is_subscribe )
 
 UPDATE users
 SET birthday = '2010-04-04'
-WHERE (gender = 'female' AND is_subscribe = 'false')
-
-
--- Вивести всіх користувачів з інформацією + вік
---  age(birthday) - функція, яка вираховує вік позичальника
-SELECT id,first_name, last_name, birthday, age(birthday)
-FROM users
--- варіант 2
-SELECT id,first_name, last_name, birthday, extract('years' from age(birthday))
-FROM users
-
-//------------------------------------------------------------------
--- make_interval(years, month, days) - функція, яка створює власний інтервал
-SELECT id,first_name, last_name, make_interval(40,8,20)
-FROM users
-//--------------------------------------------------------------------------
-
--- Знайти користувачів віком від 14 до 20 років
-SELECT id,first_name, last_name, birthday, extract('years' from age(birthday))
-FROM users
-WHERE extract('years' from age(birthday)) BETWEEN 14 AND 20
-
--- АЛІАСИ або ПСЕВДОНІМИ
--- українські слова відображаються в подвійних ліпках " "
-SELECT id AS "особливий номер", first_name AS "ім'я", last_name AS "прізвище", extract('years' from age(birthday)) AS years
+WHERE (gender = 'female'AND is_subscribe = 'false') 
+    
+    -- Вивести всіх користувачів з інформацією + вік
+    --  age(birthday) - функція, яка вираховує вік позичальника
+SELECT id,
+    first_name,
+    last_name,
+    birthday,
+    age(birthday)
 FROM users
 
-//----------------------------------------------------------------------
+ -- варіант 2
+SELECT id,
+    first_name,
+    last_name,
+    birthday,
+    extract( 'years'from age(birthday))
+FROM users 
 
---ПАГІНАЦІЯ
--- сторінки
--- яка кількість результатів буде на одній сторінці
-
--- отримати тільки 50 користувачів
-SELECT id,first_name, last_name
+// ------------------------------------------------------------------
+    -- make_interval(years, month, days) - функція, яка створює власний інтервал
+SELECT id,
+    first_name,
+    last_name,
+    make_interval(40, 8, 20)
+FROM users // --------------------------------------------------------------------------
+    
+    -- Знайти користувачів віком від 14 до 20 років
+SELECT id,
+    first_name,
+    last_name,
+    birthday,
+    extract( 'years' from age(birthday))
 FROM users
-LIMIT 50
+WHERE extract('years' from age(birthday)) BETWEEN 14 AND 20 
+    
+    
+    -- АЛІАСИ або ПСЕВДОНІМИ
+    -- українські слова відображаються в подвійних ліпках " "
+SELECT id AS "особливий номер",
+    first_name AS "ім'я",
+    last_name AS "прізвище",
+    extract( 'years'from age(birthday)) AS years
+FROM users 
+
+
+// ----------------------------------------------------------------------
+    --ПАГІНАЦІЯ
+    -- сторінки
+    -- яка кількість результатів буде на одній сторінці
+    -- отримати тільки 50 користувачів
+SELECT id,
+    first_name,
+    last_name
+FROM users
+LIMIT 50 
 
 
 -- отримати тільки 50 користувачів на другій сторінці
-SELECT id,first_name, last_name
+SELECT id,
+    first_name,
+    last_name
 FROM users
-LIMIT 50
-OFFSET 50
+LIMIT 50 OFFSET 50
 
---Як нам дізнатися скілька потрібно відступати
--- формула для розрахунку OFFSET
---  OFFSET * сторінку, яку ми запитуємо -1
+
+
+ --Як нам дізнатися скілька потрібно відступати
+    -- формула для розрахунку OFFSET
+    --  OFFSET * сторінку, яку ми запитуємо -1
+   
+   
+   
+    // == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == --створити запит з об'єднаними стовпцями
+SELECT id,
+    first_name || ' ' || last_name AS Fullname,
+    gender,
+    email
+FROM users
+
+
+ -- варіант 2
+SELECT id,
+    concat(first_name, ' ', last_name) AS "Full name",
+    gender,
+    email
+FROM users
+
+
+
+ --знайти користувачів, де Full name менше 10 символів
+SELECT id,
+    concat(first_name, ' ', last_name) AS "Full name",
+    gender,
+    email
+FROM users
+WHERE char_length(concat(first_name, ' ', last_name)) < 10
+
+
+
+--варіант 2
+SELECT *
+FROM 
+(
+        SELECT  id, concat(first_name, ' ', last_name) AS "Full name",  gender, email
+        FROM users
+    )  AS "FN"
+WHERE char_length("FN"."Full name") < 10   
