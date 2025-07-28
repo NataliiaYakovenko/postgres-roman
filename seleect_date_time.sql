87371655e6f2735889355588c372d3497e67f7b1
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-07-28 12:01:31 +0300

    Add extract to select_date_time

---
+++ seleect_date_time.sql
@@ -0,0 +1,25 @@
+-- оновлюємо дату народження чоловікам, які підписані на нашу розсилку
+UPDATE users
+SET birthday = '2000-01-01'
+WHERE (gender = 'male' AND is_subscribe)
+
+UPDATE users
+SET birthday = '2005-05-05'
+WHERE (gender = 'male' AND is_subscribe = 'false')
+
+UPDATE users
+SET birthday = '209-09-09'
+WHERE (gender = 'female' AND is_subscribe)
+
+UPDATE users
+SET birthday = '2010-04-04'
+WHERE (gender = 'female' AND is_subscribe = 'false')
+
+
+-- Вивести всіх користувачів з інформацією + вік
+--  age(birthday) - функція, яка вираховує вік позичальника
+SELECT id,first_name, last_name, birthday, age(birthday)
+FROM users
+-- варіант 2
+SELECT id,first_name, last_name, birthday, extract('years' from age(birthday))
+FROM users
