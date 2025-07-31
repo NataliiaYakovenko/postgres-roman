70074e35e7e6831042c14fc1850eadb48dda34df
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-07-31 15:47:01 +0300

    Add normalizations 1,2 forms

--- normalize.sql
+++ normalize.sql
@@ -24,3 +24,20 @@ VALUES ('Nataliia', 'JS developer'),
        ('Jeck', 'Bodyguard for developers'),
        ('Ann', 'Driver')
 
+
+ INSERT INTO employee (name,position)
+VALUES ('Naris', 'CFO'),
+       ('Serg', 'CEO'),
+       ('Jon', 'SMM/PR division'),
+       ('Adrea', 'Accountant')      
+
+ INSERT INTO positions (name,car_aviability)
+VALUES ('CFO', true),
+       ( 'CEO', true),
+       ( 'SMM/PR division', false),
+       ('Accountant', false)      
+
+
+SELECT *
+FROM employee AS e JOIN positions AS p
+ON e.position = p.name
