7ffa9a80ae46cb4c3c2524a5bbbebeaff87bf9c1
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-07-31 17:20:26 +0300

    Add normalization 3 form

--- normalize.sql
+++ normalize.sql
@@ -1,35 +1,53 @@
-DROP TABLE employee
+DROP TABLE employee, positions,departments
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     name VARCHAR(200),
-    position VARCHAR(300) REFERENCES positions(name)
+    position VARCHAR(200) REFERENCES positions(name)
 );
 
 CREATE TABLE positions(
         name VARCHAR(300) PRIMARY KEY,
-        department VARCHAR(200),
+        department VARCHAR(200) REFERENCES departments(name),
         car_aviability BOOLEAN
 )  
 
-INSERT INTO positions (name,car_aviability)
-VALUES ('JS developer', false),
-       ( 'Sales manager', false),
-       ( 'Bodyguard for developers', true),
-       ('Driver', true)
+CREATE TABLE departments (
+    name VARCHAR(200) PRIMARY KEY,
+    phone_namber VARCHAR(15)
+)       
+
+
+ALTER TABLE employee
+DROP COLUMN department_phone
+
+ALTER TABLE employee
+ADD FOREIGN KEY (department) REFERENCES departments(name)
+
+
+
+INSERT INTO positions (name,department,car_aviability)
+VALUES ('JS developer', 'Top manadgment',false),
+       ( 'Sales manager','Top manadgment', false),
+       ( 'Bodyguard for developers','Hoz department', true),
+       ('Driver','Hoz department', true),
+       ('CFO','Finantial', true),
+       ( 'CEO', 'Finantial',true),
+       ( 'SMM/PR division', 'Top manadgment',false),
+       ('Accountant','Finantial', false) 
 
 INSERT INTO employee (name,position)
 VALUES ('Nataliia', 'JS developer'),
        ('Roll', 'Sales manager'),
        ('Jeck', 'Bodyguard for developers'),
-       ('Ann', 'Driver')
-
-
- INSERT INTO employee (name,position)
-VALUES ('Naris', 'CFO'),
+       ('Ann', 'Driver'),
+       ('Naris','CFO' ),
        ('Serg', 'CEO'),
        ('Jon', 'SMM/PR division'),
        ('Adrea', 'Accountant')      
+
+
+     
 
  INSERT INTO positions (name,car_aviability)
 VALUES ('CFO', true),
@@ -38,6 +56,20 @@ VALUES ('CFO', true),
        ('Accountant', false)      
 
 
+
+INSERT INTO departments (name,phone_namber)
+VALUES ('Top manadgment', '234-55-66'),
+       ('Finantial','222-66-77'),
+       ('Hoz department', '999-44-55')
+     
+
 SELECT *
 FROM employee AS e JOIN positions AS p
 ON e.position = p.name
+
+SELECT *
+FROM employee AS e JOIN positions AS p
+ON e.position = p.name
+                   JOIN departments AS d
+                   ON  p.department = d.name
+
